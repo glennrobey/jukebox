83ea02a07e6b6b03edec29d569060fba708c6b30
Glenn Robey <glennrobey95@gmail.com>
2025-10-28 15:01:14 -0500

    complete!! (fixed typo)

--- db/schema.sql
+++ db/schema.sql
@@ -23,28 +23,4 @@ CREATE TABLE playlists_tracks (
     playlist_id INTEGER NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
     track_id INTEGER NOT NULL REFERENCES tracks(id) oN DELETE CASCADE,
     unique(playlist_id, track_id)
-);DROP TABLE IF EXISTS playlists_tracks;
-DROP TABLE IF EXISTS playlists;
-DROP TABLE IF EXISTS tracks;
-
--- Tracks Table
-CREATE TABLE tracks (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    duration_ms INTEGER NOT NULL
-);
-
--- Playlists Table
-CREATE TABLE playlists (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL
-);
-
--- Junction Table
-CREATE TABLE playlists_tracks (
-    id SERIAL PRIMARY KEY,
-    playlist_id INTEGER NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
-    track_id INTEGER NOT NULL REFERENCES tracks(id) ON DELETE CASCADE,
-    UNIQUE(playlist_id, track_id)
 );
